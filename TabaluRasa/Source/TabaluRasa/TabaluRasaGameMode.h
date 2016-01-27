000a44e29fa6fa27ed3bdac2746d3065ad9ba17d
Halliom <johan.jww@hotmail.com>
2016-01-27 19:48:58 +0100

    First commit!

---
+++ TabaluRasa/Source/TabaluRasa/TabaluRasaGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TabaluRasaGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TABALURASA_API ATabaluRasaGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
