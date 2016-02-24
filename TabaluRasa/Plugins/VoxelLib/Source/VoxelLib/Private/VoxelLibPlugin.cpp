39a9873f875242739025b118fb82f1301a758d57
Halliom <johan.jww@hotmail.com>
2016-02-24 17:37:40 +0100

    Removed unreal engine version

--- TabaluRasa/Plugins/VoxelLib/Source/VoxelLib/Private/VoxelLibPlugin.cpp
+++
@@ -1,5 +0,0 @@
-#include "VoxelLibPluginPrivatePCH.h"
-
-IMPLEMENT_MODULE(IVoxelLibPlugin, VoxelLib)
-
-DEFINE_LOG_CATEGORY(VoxelLog);
--- TabaluRasa/Plugins/VoxelLib/Source/VoxelLib/Public/VoxelLibPluginPrivatePCH.h
+++
@@ -1,8 +0,0 @@
-#include "CoreUObject.h"
-#include "Engine.h"
-
-#include "Chunk.h"
-
-#define println(print_str) UE_LOG(VoxelLog, Warning, TEXT(print_str));
-
-#include "IVoxelLibPlugin.h"
