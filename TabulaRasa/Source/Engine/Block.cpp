dc2fb676c6a234981d60d75ec09e1774c4f8dd26
Halliom <johan.jww@hotmail.com>
2016-03-04 14:36:00 +0100

    Added block class for rendering purposes

---
+++ TabulaRasa/Source/Engine/Block.cpp
@@ -0,0 +1,12 @@
+#include "Block.h"
+
+BlockInfo BlockManager::LoadedBlocks[MAX_NUM_BLOCKS];
+
+#define ADD_SOLID(Id, TextureEast, TextureWest, TextureTop, TextureBottom, TextureNorth, TextureSouth) \
+		{Id, {TextureEast, TextureWest, TextureTop, TextureBottom, TextureNorth, TextureSouth}, RENDER_TYPE_SOLID}
+
+void BlockManager::SetupBlocks()
+{
+	LoadedBlocks[BLOCK_ID_GRASS] = ADD_SOLID(BLOCK_ID_GRASS, 0, 0, 0, 0, 0, 0);
+	LoadedBlocks[BLOCK_ID_DIRT] = ADD_SOLID(BLOCK_ID_DIRT, 0, 0, 0, 0, 0, 0);
+}
---
+++ TabulaRasa/Source/Engine/Block.h
@@ -0,0 +1,42 @@
+
+#define MAX_NUM_BLOCKS 4096
+
+#define BLOCK_ID_GRASS 0
+#define BLOCK_ID_DIRT 1
+
+enum BlockRenderType
+{
+	RENDER_TYPE_SOLID,
+	RENDER_TYPE_CUSTOM
+};
+
+struct BlockInfo
+{
+	/**
+	 * Id of the block, this is used when storing
+	 * the block and also goes for blocks that require
+	 * custom rendering
+	 */
+	unsigned int BlockID;
+
+	/**
+	 * Texture index for each side
+	 */
+	unsigned int Textures[6];
+
+	/**
+	 * The rendertype for this block, if it should
+	 * be rendered normally or if some kind of custom 
+	 * rendering should be done
+	 */
+	BlockRenderType RenderType;
+};
+
+class BlockManager
+{
+public:
+
+	static void SetupBlocks();
+
+	static BlockInfo LoadedBlocks[MAX_NUM_BLOCKS];
+};
