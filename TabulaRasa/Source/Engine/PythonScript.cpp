28c89cf418f6f1a6d1850ba89dbf51cbd0964610
Halliom <johan.jww@hotmail.com>
2016-04-02 11:30:29 +0200

    Started work on executing scripts

--- TabulaRasa/Source/Engine/PythonScript.cpp
+++ TabulaRasa/Source/Engine/PythonScript.cpp
@@ -2,11 +2,9 @@
 
 #include "Console.h"
 
-static char string[1024];
-
 extern Console* g_Console;
 
-PyObject* aview_write(PyObject* self, PyObject* args)
+PyObject* __cdecl Write(PyObject* self, PyObject* args)
 {
 	const char* Output;
 	if (!PyArg_ParseTuple(args, "s", &Output))
@@ -16,9 +14,19 @@ PyObject* aview_write(PyObject* self, PyObject* args)
 	return Py_BuildValue("");
 }
 
-PyObject* aview_flush(PyObject* self, PyObject* args)
+PyObject* __cdecl Flush(PyObject* self, PyObject* args)
 {
 	return Py_BuildValue("");
+}
+
+PyObject* aview_write(PyObject* self, PyObject* args)
+{
+	return Write(self, args);
+}
+
+PyObject* aview_flush(PyObject* self, PyObject* args)
+{
+	return Flush(self, args);
 }
 
 PyMethodDef aview_methods[] =
@@ -48,14 +56,16 @@ PyMODINIT_FUNC PyInit_aview(void)
 
 void PythonScriptEngine::Initialize()
 {
-	freopen(NULL, "a", stdout);
-	setbuf(stdout, string);
+	Py_Initialize();
+
+	m_InterpreterContext = { 0 };
+	m_InterpreterContext.Main = PyImport_ImportModule("__main__");
+	m_InterpreterContext.Locals = PyModule_GetDict(m_InterpreterContext.Main);
+	//PyObject_GetAttr();
 
 	PyImport_AppendInittab("aview", PyInit_aview);
-	Py_Initialize();
 	PyImport_ImportModule("aview");
-
-	PyRun_SimpleString("import aview");
+	PyRun_String("import aview", Py_single_input, m_InterpreterContext.Locals, m_InterpreterContext.Locals);
 }
 
 void PythonScriptEngine::Destroy()
@@ -65,25 +75,54 @@ void PythonScriptEngine::Destroy()
 
 void PythonScriptEngine::ExecuteStringInInterpreter(char* String)
 {
-	PyRun_SimpleString(String);
-}
-
-char *PythonScriptEngine::GetVariableValue(char* VariableName)
-{
-	PyObject* MainModule = PyImport_AddModule("__main__");
-	PyObject* ModuleDictionary = PyModule_GetDict(MainModule);
-
-	PyObject* Result = PyDict_GetItemString(ModuleDictionary, VariableName);
+	PyObject* Result = PyRun_String(String, Py_single_input, m_InterpreterContext.Locals, m_InterpreterContext.Locals);
 	if (!Result)
-		return "";
-	
+	{
+
+	}
 	PyObject* ObjectRepr = PyObject_Repr(Result);
 	PyObject* Unicode = PyUnicode_AsUTF8String(ObjectRepr);
-	char* String= PyBytes_AsString(Unicode);
+	char* PythonResultString = PyBytes_AsString(Unicode);
+	unsigned int StringLength = strlen(PythonResultString);
+	char* ResultString = new char[StringLength];
+
+	strcpy(ResultString, PythonResultString);
+	Py_DecRef(Unicode);
+	Py_DecRef(Result);
+}
+
+char* PythonScriptEngine::GetVariableValue(char* VariableName)
+{
+	PyObject* Result = PyDict_GetItemString(m_InterpreterContext.Locals, VariableName);
+	if (!Result)
+		return "";
+
+	PyObject* ObjectRepr = PyObject_Repr(Result);
+	PyObject* Unicode = PyUnicode_AsUTF8String(ObjectRepr);
+	char* String = PyBytes_AsString(Unicode);
 	unsigned int StringLength = strlen(String);
 	char* ResultString = new char[StringLength];
 
 	strcpy(ResultString, String);
 	Py_DECREF(Unicode);
 	return ResultString;
+}
+
+PythonScript PythonScriptEngine::CreateScript(char* ScriptName, char* ScriptString)
+{
+	PythonScript NewScript = { 0 };
+	//PyParser_SimpleParseStringFlagsFilename
+	PyObject* CompiledScript = Py_CompileString(ScriptString, NULL, Py_file_input);
+
+	if (CompiledScript == NULL)
+	{
+		//TODO: Compilation error
+	}
+	NewScript.CompiledSource = CompiledScript;
+
+	NewScript.Context.Main = PyImport_ImportModule("__main__");
+	NewScript.Context.Locals = PyModule_GetDict(m_InterpreterContext.Main);
+	PyRun_String("import aview", Py_single_input, NewScript.Context.Locals, NewScript.Context.Locals);
+
+	return NewScript;
 }
--- TabulaRasa/Source/Engine/PythonScript.h
+++ TabulaRasa/Source/Engine/PythonScript.h
@@ -6,6 +6,37 @@
 #define _DEBUG 1
 #endif
 
+struct PythonContext
+{
+	PyObject* Main;
+	PyObject* Locals;
+};
+
+struct PythonScript
+{
+	PythonContext Context;
+
+	PyObject* CompiledSource;
+};
+
+struct PythonReference
+{
+	PythonReference(PyObject* Object) : Object(Object) { ReferenceCount = new int; *ReferenceCount = 1; }
+	PythonReference(PyObject* Object, int* ReferenceCount) : Object(Object), ReferenceCount(ReferenceCount) { ++(*ReferenceCount); }
+
+	~PythonReference() 
+	{ 
+		if (--(*ReferenceCount) == 0)
+		{
+			Py_DecRef(Object);
+		}
+	}
+
+	PyObject* Object;
+
+	int* ReferenceCount;
+};
+
 class PythonScriptEngine
 {
 public:
@@ -14,7 +45,14 @@ public:
 
 	void Destroy();
 
-	void ExecuteStringInInterpreter(char *String);
+	void ExecuteStringInInterpreter(char* String);
 
-	char *GetVariableValue(char *VariableName);
+	char* GetVariableValue(char* VariableName);
+
+	PythonScript CreateScript(char* ScriptName, char* ScriptString);
+
+	void ExecuteScript();
+	
+	PythonContext m_InterpreterContext;
+
 };
