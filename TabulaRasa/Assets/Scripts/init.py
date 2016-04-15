4ffa5354bd8fab2479ef0164654347647ad19929
Halliom <johan.jww@hotmail.com>
2016-04-15 16:26:55 +0200

    Changed the sizes to be more aesthetically pleasing

--- TabulaRasa/Assets/Scripts/init.py
+++ TabulaRasa/Assets/Scripts/init.py
@@ -1,5 +1,5 @@
 import engine
 
 if __name__ == "__main__":
-    engine.load_font("TitilliumWeb-Regular.ttf", 16)
-    engine.load_font("RobotoMono-Regular.ttf", 24)
+    engine.load_font("TitilliumWeb-Regular.ttf", 20)
+    engine.load_font("RobotoMono-Regular.ttf", 18)
