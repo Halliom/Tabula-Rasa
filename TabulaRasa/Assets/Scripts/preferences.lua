63c34f97442433cd22692adc698efed44b738cb2
partaviom <david.mastermind@live.se>
2016-05-17 18:55:34 +0200

    Added Preferences

---
+++ TabulaRasa/Assets/Scripts/preferences.lua
@@ -0,0 +1,18 @@
+--Preferences
+
+--screen sizes
+local widths = 1920
+local height = 1080
+
+--vsync
+ local vsync = True
+
+--FOV
+local fov = 60    -- 0-100
+
+--sound level
+local sound_level = 100   -- 0-100
+
+--graphics settings??
+
+--inputs??
