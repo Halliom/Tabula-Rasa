59596c9b1160093c6ae0a9f65ef42b11ed5b2eec
partaviom <david.mastermind@live.se>
2016-05-18 22:32:26 +0200

    Preferences Updat

--- TabulaRasa/Assets/Scripts/preferences.lua
+++ TabulaRasa/Assets/Scripts/preferences.lua
@@ -1,11 +1,12 @@
 --Preferences
 
---screen sizes
-widths = 1920
-height = 1080
-
+Window = {
+  title = "Tabula Rasa"
+  widths = 1920
+  height = 1080
+}
 --vsync
-vsync = True
+vsync = true
 
 --FOV
 fov = 60    -- 0-100
@@ -15,4 +16,4 @@ sound_level = 100   -- 0-100
 
 --graphics settings??
 
---inpu
+--inputs
