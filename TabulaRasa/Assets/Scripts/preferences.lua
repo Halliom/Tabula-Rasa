b8a6a6603e4de3a357dbd95848c446bee29ac1cc
partaviom <david.mastermind@live.se>
2016-05-20 14:36:58 +0200

    Preferences Updat

--- TabulaRasa/Assets/Scripts/preferences.lua
+++ TabulaRasa/Assets/Scripts/preferences.lua
@@ -1,15 +1,15 @@
 --Preferences
 
 Window = {
-  title = "Tabula Rasa"
-  widths = 1280
-  height = 720
+  title = "Tabula Rasa",
+  width = 1280,
+  height = 720,
+  vsync = true
 }
---vsync
-vsync = true
 
---FOV
-fov = 60    -- 0-100
+Game_settings = {
+  fov = 60    -- 0-100
+}
 
 --sound level
 sound_level = 100   -- 0-100
