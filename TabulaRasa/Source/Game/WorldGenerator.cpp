80e69f854a4c301af3df4aefa4b240d5cd15dceb
Halliom <johan.jww@hotmail.com>
2016-06-05 17:55:41 +0200

    Started work on World generation

--- TabulaRasa/Source/Game/WorldGenerator.cpp
+++ TabulaRasa/Source/Game/WorldGenerator.cpp
@@ -1,2 +1,51 @@
 #include "WorldGenerator.h"
 
+#include "..\Engine\Noise.h"
+#include "..\Game\World.h"
+
+BaseTerrain::~BaseTerrain()
+{
+}
+
+void BaseTerrain::GenerateToChunk(SimplexNoise* NoiseGenerator)
+{
+
+}
+
+WorldGenerator::WorldGenerator(int Seed, World* WorldObject) : 
+	m_Seed(Seed),
+	m_WorldObject(WorldObject),
+	m_NoiseGenerator(new SimplexNoise(Seed)),
+	m_HeatmapGenerator(new SimplexNoise(Seed * 1337))
+{
+}
+
+WorldGenerator::~WorldGenerator()
+{
+	delete m_NoiseGenerator;
+}
+
+void WorldGenerator::GenerateChunk(glm::ivec2 ChunkPosition)
+{
+}
+
+ChunkGenerator::ChunkGenerator() : 
+	m_NumFeatures(0)
+{
+}
+
+ChunkGenerator::~ChunkGenerator()
+{
+	for (int i = 0; i < m_NumFeatures; ++i)
+	{
+		delete m_Features[i];
+	}
+}
+
+void ChunkGenerator::AddFeatureGenerator(IFeature* Generator)
+{
+	// Make sure that we never have more than the maximum amount
+	assert(m_NumFeatures < 8);
+	// Insert the Generator into the list
+	m_Features[m_NumFeatures++] = Generator;
+}
--- TabulaRasa/Source/Game/WorldGenerator.h
+++ TabulaRasa/Source/Game/WorldGenerator.h
@@ -1,13 +1,58 @@
 #pragma once
 
+#include "glm\common.hpp"
+
+class IFeature
+{
+public:
+
+	IFeature() {};
+
+	virtual ~IFeature() {}
+
+	virtual void GenerateToChunk(class SimplexNoise* NoiseGenerator) = 0;
+
+};
+
+class BaseTerrain : public IFeature
+{
+public:
+
+	virtual ~BaseTerrain() override;
+
+	virtual void GenerateToChunk(class SimplexNoise* NoiseGenerator) override;
+};
+
+class Biome
+{
+
+};
+
+class ChunkGenerator
+{
+public:
+
+	ChunkGenerator();
+
+	~ChunkGenerator();
+
+	void AddFeatureGenerator(IFeature* Generator);
+
+	class IFeature*		m_Features[8];
+	int					m_NumFeatures;
+};
+
 class WorldGenerator
 {
 public:
-	WorldGenerator();
+	WorldGenerator(int Seed, class World* WorldObject);
 
 	~WorldGenerator();
 
-	void GenerateChunk();
+	void GenerateChunk(glm::ivec2 ChunkPosition);
 
-	int Seed;
+	int					m_Seed;
+	class World*		m_WorldObject;
+	class SimplexNoise* m_NoiseGenerator;
+	class SimplexNoise* m_HeatmapGenerator;
 };
