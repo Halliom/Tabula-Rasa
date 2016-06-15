15b2b742ed9e3659566e9288b1ed8283e0b6a680
Halliom <johan.jww@hotmail.com>
2016-06-15 11:28:42 +0200

    Generators are now defined as tables and can be defined anywhere inside Scripts/WorldGen/

--- TabulaRasa/Assets/Scripts/WorldGen/biome_test.lua
+++ TabulaRasa/Assets/Scripts/WorldGen/biome_test.lua
@@ -1,13 +1,19 @@
 BiomeTest = {
 	min_heat = 0,
 	max_heat = 50,
-	min_height = -128,
-	max_height = -1,
+	min_height = -64,
+	max_height = 1,
+	features = {
+		"BaseTerrain"
+	}
+}
+
+BaseTerrain = {
 	generate = function(simplex_noise, chunk, world_x, world_y, world_z)
 		for i = 0, 31 do
 			for j = 0, 31 do
 				local Y = simplex_noise:noise((world_x + i) / 50.0, (world_z + j) / 50.0) * 10
-				for k = 0, Y do
+				for k = 0, 31 do
 					if (world_y + k) <= Y then
 						chunk:set_block(i, k, j, 1)
 					end
