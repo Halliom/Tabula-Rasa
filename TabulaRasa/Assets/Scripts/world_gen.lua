efd171f3cc8ee239e3a7b672ada9f7267d6d61d0
partaviom <david.mastermind@live.se>
2016-06-21 18:16:25 +0200

    Updata

--- TabulaRasa/Assets/Scripts/world_gen.lua
+++ TabulaRasa/Assets/Scripts/world_gen.lua
@@ -1,30 +1,24 @@
-function print_two(a, b)
+BiomeTest = {
+	min_heat = 0,
+	max_heat = 50,
+	min_height = -64,
+	max_height = 1,
+	features = {
+		"BaseTerrain"
+	}
+}
 
-  print(a.." "..b)
-
-end
-
-function gen_world()
-
-  local r = 8
-  local i = 1
-  local j = 1
-  local y = 1
-  local x = 1
-
-  while i <= 2*r do
-
-    while j <= 2*r do
-      print_two(j, i)
-
-      if r*r == (i - y)*(i - y) + (j - x)*(j - x) then
-        world.add_block(j, 1, i, 1)
-        print_two(j, i)
-      end
-
-      j = j + 1
-    end
-
-    i = i + 1
-  end
-end
+BaseTerrain = {
+	generate = function(simplex_noise, chunk, world_x, world_y, world_z)
+		for i = 0, 31 do
+			for j = 0, 31 do
+				local Y = simplex_noise:noise((world_x + i) / 50.0, (world_z + j) / 50.0) * 10
+				for k = 0, 31 do
+					if (world_y + k) <= Y then
+						chunk:set_block(i, k, j, 1)
+					end
+				end
+			end
+		end
+	end
+}
