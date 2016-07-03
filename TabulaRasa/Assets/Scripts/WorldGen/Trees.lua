438bc563155a303733fe725ee9314f2f87295a30
partaviom <david.mastermind@live.se>
2016-07-03 12:31:00 +0200

    More work on Trees

--- TabulaRasa/Assets/Scripts/WorldGen/Trees.lua
+++ TabulaRasa/Assets/Scripts/WorldGen/Trees.lua
@@ -1,1 +1,38 @@
+-- radien
+local r = 4
 
+-- mitt punkten av cirklen
+local y0 = 0
+local x0 = 0
+local z0 = 0
+
+-- antalet pungter
+local i = 11
+
+-- en pungt på cirklen rand
+y =
+x =
+z =
+
+-- beräkna max y, x, z
+-- r^2 = (y - y0)^2 + (x - x0)^2 + (z - z0)^2
+
+
+-- genererar random pungter
+local p_generation = function
+  local k = {}
+  local var = false
+  while var = false do
+    local y = math.random(0, r)
+    local x = math.random(0, r)
+    local z = math.random(0, r)
+    if r^2 = (y - y0)^2 + (x - x0)^2 + (z - z0)^2 then
+      var = true
+    end
+  end
+end
+
+
+while  do
+  -- body...
+end
