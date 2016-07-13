5db65a89542b3bc3fb177ac6ab377a691327edbe
partaviom <David@Christers-MacBook-Air.local>
2016-07-13 19:32:45 +0200

    More work on trees

--- TabulaRasa/Assets/Scripts/WorldGen/Trees.lua
+++ TabulaRasa/Assets/Scripts/WorldGen/Trees.lua
@@ -2,27 +2,38 @@
 local r = 4
 
 -- mitt punkten av cirklen
-local y0 = 0
 local x0 = 0
+local y0 = 0
 local z0 = 0
 
 -- antalet pungter
 local i = 11
-local j = 0
+local j = 1
 
 -- pungter inom randen
 local list = {}
+
+-- sublits function
+function coord(x, y, z)
+  local result = {}
+  result.x = x
+  result.y = y
+  result.z = z
+  result.d = 0
+  result.v = 0
+  return result
+end
 
 -- genererar random pungter
 function p_generation()
   local k = {}
   local var = false
   while var == false do
-    local y = math.random(0, r)
     local x = math.random(0, r)
+    local y = math.random(0, r)
     local z = math.random(0, r)
-    if r^2 >= (y - y0)^2 + (x - x0)^2 + (z - z0)^2 then
-      k = {y, x, z}
+    if r^2 >= (x - x0)^2 + (y - y0)^2 + (z - z0)^2 then
+      k = coord(x, y, z)
       var = true
       return k
     end
@@ -30,17 +41,37 @@ function p_generation()
 end
 
 while j <= i do
-  list = list + p_generation()
-  j =+ 1
+  table.insert(list, p_generation())
+  j = j + 1
 end
 
 -- soterar list efter punkternas avstånd från centrum
-j = 0
+j = 1
 while j <= i do
-  local y = list[j[0]]
-  local x = list[j[1]]
-  local z = list[j[2]]
-  local distance = math.sqrt((y - y0)^2 + (x - x0)^2 + (z - z0)^2)
-  list[j[3]] = distance
-  j =+ 1
+  local x = list[j].x
+  local y = list[j].y
+  local z = list[j].z
+  local distance = math.sqrt((x - x0)^2 + (y - y0)^2 + (z - z0)^2)
+  list[j].d = distance
+  j = j + 1
+end
+
+function comapare(a, b)
+  return a.d < b.d
+end
+
+for k, g in pairs(list) do
+  for h, l in pairs(g) do
+    print(h.." : "..l)
+  end
+end
+
+table.sort(list, comapare)
+
+print("----------------")
+
+for k, g in pairs(list) do
+  for h, l in pairs(g) do
+    print(h.." : "..l)
+  end
 end
